ddaaef8656d5408ee25f37887168644738c86726
Zoey Guo <zoey-guo@uiowa.edu>
2024-07-11 09:55:49 -0500

    added filtering to wishlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
-import { NgIf } from '@angular/common';
+import { FormsModule }   from '@angular/forms'; // needed to use NgModel
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete,
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf],
+  imports: [RouterOutlet, NgFor, NgIf, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   items : WishItem[] = [
     new WishItem('Learn Angular'),
-    new WishItem('Get Coffee', true),
+    new WishItem('Get coffee', true),
     new WishItem('Find grass that cuts itself')
-  ]
+  ];
+
+  listFilter : any = '0';
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toggleItem(item : WishItem) {
+    item.isComplete = !item.isComplete;
+  }
 }
